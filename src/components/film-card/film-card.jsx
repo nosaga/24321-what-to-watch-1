db520bf74a0a38be660d52646a3e52afe87deae2
germiona <violetta.ashirova@gmail.com>
2019-05-12 13:56:14 +0300

    fixes

--- src/components/film-card/film-card.jsx
+++ src/components/film-card/film-card.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Card from "../card/card.jsx";
 import filmCards from "../../data/films-cards.js";
 
-const FilmCard = ({onClick}) => {
-  return (
-    filmCards.map((title) => {
-      return (
-        <article className="small-movie-card catalog__movies-card" key={title}>
-          <button className="small-movie-card__play-btn" type="button">Play</button>
-          <div className="small-movie-card__image">
-            <img src="img/bohemian-rhapsody.jpg"
-              alt="" width="280" height="175"/>
-          </div>
-          <h3 className="small-movie-card__title">
-            <a className="small-movie-card__link" onClick={onClick} href="movie-page.html">{title}</a>
-          </h3>
-        </article>
-      );
-    })
-  );
-};
+
+const FilmCard = ({onClick}) => filmCards.map((title) =>
+  <Card key={title}
+    title={title}
+    onClick={onClick}
+  />
+);
 
 FilmCard.propTypes = {
   onClick: PropTypes.func
 };
 
 export default FilmCard;
+
+
